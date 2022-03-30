c3d070475ceb441a56b428e87f0566c13b656f15
David Fernandez Belden <DavidFDot@gmail.com>
2022-03-29 22:41:31 -0600

    final push act 10

--- token_weight.py
+++ token_weight.py
@@ -8,11 +8,20 @@ posting_files = open("Activity_10_files/diccionario.html", 'w')
 for key, value in hash_dic.items():
 
     listOfInts = value.split(",")
-    print("listOfInts: " + listOfInts)
-    numRepeticiones = int(listOfInts[1])
-    numTokens = int(listOfInts[2])
-    resultado = 0 if numTokens == 0 else numRepeticiones * 100 / numTokens
-    dic[key] = resultado
-    posting_files.write('\n'.join(str(key) + ": " + str(resultado)))
+    try:
+        numRepeticiones = int(listOfInts[1])
+        numTokens = int(listOfInts[2])
+        resultado = 0 if numTokens == 0 else numRepeticiones * 100 / numTokens
+        dic[key] = resultado
+    except:
+        print("Se presento una excepcion")
+
+nuevoArr = []
+
+for key, value in dic.items():
+    nuevoArr.append((str(key) + ": " + str(resultado)))
+
+posting_files.write('\n'.join(nuevoArr))
 
 posting_files.close()
+
